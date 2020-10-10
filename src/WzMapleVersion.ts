185e3ab950141e53e25f35e3f8ecdd35011cd56c
toyobayashi <lifenglin314@outlook.com>
2020-10-11 01:17:46 +0800

    WzMapleVersion comment

--- src/WzMapleVersion.ts
+++ src/WzMapleVersion.ts
@@ -2,12 +2,33 @@
  * @public
  */
 export enum WzMapleVersion {
+  /**
+   * Global MapleStory (old)
+   */
   GMS = 0,
+
+  /**
+   * 新楓之谷 / 冒险岛Online / 메이플스토리 / MapleSEA / EMS (old)
+   */
   EMS = 1,
+
+  /**
+   * BMS / GMS / MapleSEA / メイプルストーリー / 메이플스토리
+   */
   BMS = 2,
+
   CLASSIC = 3,
+
+  /**
+   * 生成
+   */
   GENERATE = 4,
+
   GETFROMZLZ = 5,
+
+  /**
+   * 自定义编码
+   */
   CUSTOM = 6,
 
   UNKNOWN = 99,
