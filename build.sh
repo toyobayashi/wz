16e09ec211142aeca76d3659cffc02f8f6a58b78
toyobayashi <lifenglin314@outlook.com>
2020-10-16 23:52:40 +0800

    build zlib

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+type="Release"
+
+until [ $# -eq 0 ]
+do
+if [ "$1" == "Release" ]; then type="$1"; fi
+if [ "$1" == "Debug" ]; then type="$1"; fi
+shift
+done
+
+cd ./zlibwasm
+cmakeoutdir="cmake_build"
+mkdir -p $cmakeoutdir
+cd $cmakeoutdir
+cmake -DCMAKE_TOOLCHAIN_FILE="$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" -DCMAKE_BUILD_TYPE="$type" -G "Unix Makefiles" ..
+cmake --build .
+
+cp -rpf $cmakeoutdir/zlibwasm.js ../src/util/zlibwasm.js
+mkdir -p ../lib/cjs-modern/util
+cp -rpf $cmakeoutdir/zlibwasm.js ../lib/cjs-modern/util/zlibwasm.js
+cp -rpf $cmakeoutdir/zlibwasm.wasm ../lib/cjs-modern/util/zlibwasm.wasm
+cd ..
