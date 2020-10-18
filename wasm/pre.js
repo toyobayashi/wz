069b01520b5211c6f6dfdf61715e5f8151569795
toyobayashi <lifenglin314@outlook.com>
2020-10-18 21:47:35 +0800

    use __webpack_public_path__ to check webpack

--- wasm/pre.js
+++ wasm/pre.js
@@ -29,7 +29,7 @@
     g = g || new Function('return this')();
   } catch (_) {
     if (typeof globalThis !== 'undefined') return globalThis;
-    if (typeof __webpack_modules__ === 'undefined') {
+    if (typeof __webpack_public_path__ === 'undefined') {
       if (typeof global !== 'undefined') return global;
     }
     if (typeof window !== 'undefined') return window;
