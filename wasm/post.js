fa822b086fe78584f7e0e667f73c55bab4213937
toyobayashi <lifenglin314@outlook.com>
2020-10-18 12:14:06 +0800

    reject wasm init error

--- wasm/post.js
+++ wasm/post.js
@@ -1,2 +1,18 @@
-var _modulePromise=new Promise(function(r){Module.onRuntimeInitialized=function(){r(Module)}});return {init:function(){return _modulePromise},mod:Module};
+var _modulePromise = new Promise(function(resolve, reject) {
+  var onAbort = Module.onAbort;
+  Module.onAbort = function (message) {
+    Module.onAbort = onAbort;
+    reject(new Error(message));
+  };
+  Module.onRuntimeInitialized = function () {
+    Module.onAbort = onAbort;
+    resolve(Module);
+  };
+});
+return {
+  init: function () {
+    return _modulePromise;
+  },
+  mod: Module
+};
 });
