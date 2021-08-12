105b09d48fbba91d131a7c5b3e1f54bdd1d30be6
toyobayashi <lifenglin314@outlook.com>
2021-08-12 23:28:49 +0800

    types: support override emscripten module object

--- src/util/wz.d.ts
+++ src/util/wz.d.ts
@@ -1,9 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="emscripten" />
+
 declare namespace mod {
   export function inflate (data: Uint8Array, len: number): Uint8Array
   export function aesEnc (data: Uint8Array, key: Uint8Array): Uint8Array
 }
 
-declare function init (): Promise<{ Module: typeof mod }>
+declare function init (moduleOverrides?: Partial<EmscriptenModule>): Promise<{ Module: typeof mod }>
 
 export declare const inflate: typeof mod.inflate
 export declare const aesEnc: typeof mod.aesEnc
